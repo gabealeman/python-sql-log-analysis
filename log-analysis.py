918b2da1fa53b1f6754975f205b1f3a1bb34bf19
gabehou <ga9065@gmail.com>
2017-07-23 12:49:06 -0500

    added most popular function

--- log-analysis.py
+++ log-analysis.py
@@ -7,5 +7,28 @@ def connect(database_name="news"):
         db = psycopg2.connect("dbname={}".format(database_name))
         cursor = db.cursor()
         return db, cursor
+        print("It worked")
     except:
         print("Error! Could not connect to database")
+
+# 1. What are the most popular three articles of all time?
+
+def most_popular():
+    db, c = connect()
+    query = """
+               SELECT articles.title, count(log.path) as popularity
+               FROM articles LEFT JOIN log
+               ON log.path like CONCAT('%', articles.slug, '%')
+               WHERE log.status != '404 NOT FOUND'
+               GROUP BY articles.title
+               ORDER BY popularity DESC
+               LIMIT 3
+           """
+    c.execute(query)
+    result = c.fetchall()
+    db.close()
+    print("1. What are the most popular three articles of all time?")
+    for r in result:
+        print("%s - %s views" % (r[0], r[1]))
+
+most_popular()
